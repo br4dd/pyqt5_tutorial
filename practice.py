1f2d435ae0b2723886efd0d40fbc2451e41996eb
br4dd <earljohn321123@gmail.com>
2021-11-28 12:39:12 +0800

    simple design

--- practice.py
+++ practice.py
@@ -6,12 +6,30 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 
-def the_button_1(win):
+def button_launch(win):
     button = QPushButton(win)
-    button.setText("Name")
-    button.setToolTip("YOU HAVE DISCOVERED THE HIDDEN TREASURE")
+    button.setText('LAUNCH')
+    button.move(450,150)
 
+def button_register(win):
+    button = QPushButton(win)
+    button.setText('REGISTER A FACE')
+    button.move(450,200)
 
+def button_records(win):
+    button = QPushButton(win)
+    button.setText('RECORDS')
+    button.move(600,150)
+
+def button_logs(win):
+    button = QPushButton(win)
+    button.setText('LOGS')
+    button.move(600,200)
+
+def button_get_started(win):
+    button = QPushButton(win)
+    button.setText('GET STARTED')
+    button.move(525,250)
 
 def the_window_style(win):
         # x, y, w, h
@@ -20,13 +38,18 @@ def the_window_style(win):
     win.setStyleSheet('background:#89d5c2')
         
         # frameless
-    #win.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+    win.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 
 
-def the_title(win):
+def label_title(win):
     label = QLabel(win)
-    label.setText('HELLO WORLD')
+    label.setText('UNMASKED')
 
+
+def label_footer(win):
+    label = QLabel(win)
+    label.setText('Powered by CloverByte, 2021')
+    label.move(300,480)
 
 def run_application():
     app = QApplication(sys.argv)
@@ -36,11 +59,20 @@ def run_application():
     the_window_style(win)
 
         # window title
-    the_title(win)
+    label_title(win)
+
+    button_launch(win)
+    button_register(win)
+    button_records(win)
+    button_logs(win)
+    button_get_started(win)
+    
+    label_footer(win)
 
     win.show()
     sys.exit(app.exec_())
-
     
+
+
     # run application
 run_application()
