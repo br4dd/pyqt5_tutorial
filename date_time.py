49356e69c21cda45b2ddeb8c5f408adbc7e3563c
br4dd <earljohn321123@gmail.com>
2021-11-28 10:20:27 +0800

    first commit

---
+++ date_time.py
@@ -0,0 +1,15 @@
+from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
+
+# print(QDate.currentDate())
+# print(QDate.currentDate().toString(Qt.ISODate))
+# print(QDate.currentDate().toString(Qt.DefaultLocaleLongDate))
+
+# time = QDateTime.currentDateTime()
+# print(time)
+
+# time = QTime.currentTime()
+# print(time)
+# print(time.toString(Qt.ISODate))
+
+# print(QDate(2000,6,6).daysInMonth())
+# print(QDate(2000,6,6).daysInYear())
